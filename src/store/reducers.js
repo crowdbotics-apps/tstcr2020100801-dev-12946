0e20f686fccd9b60311d94204ba854aaed86e694
Crowdbotics <team@crowdbotics.com>
2020-10-09 15:36:33 +0000

    Update store

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,9 +1,20 @@
 import * as types from "./constants"
 
-const initialState = { testConnector: [], registration: [] }
+const initialState = {
+  test: [],
+  testConnector: [],
+  registration: [],
+  tstcrAPI: []
+}
 
 export default function apiReducer(state = initialState, action) {
   switch (action.type) {
+    case types.TEST2_POST__CREATE:
+    case types.TEST2_POST__CREATE_SUCCEEDED:
+    case types.TEST2_POST__CREATE_FAILED:
+      return Object.assign({}, state, {
+        test: [...state.test, action.response]
+      })
     case types.TESTCONNECTOR_POST__CREATE:
     case types.TESTCONNECTOR_POST__CREATE_SUCCEEDED:
     case types.TESTCONNECTOR_POST__CREATE_FAILED:
@@ -15,6 +26,132 @@ export default function apiReducer(state = initialState, action) {
     case types.REGISTRATION_POST_RESTAUTH_REGISTRATION_CREATE_FAILED:
       return Object.assign({}, state, {
         registration: [...state.registration, action.response]
+      })
+    case types.API_V1_CUSTOMTEXT_LIST:
+    case types.API_V1_CUSTOMTEXT_LIST_SUCCEEDED:
+    case types.API_V1_CUSTOMTEXT_LIST_FAILED:
+      return Object.assign({}, state, {
+        tstcrAPI: [...state.tstcrAPI, action.response]
+      })
+    case types.API_V1_CUSTOMTEXT_READ:
+    case types.API_V1_CUSTOMTEXT_READ_SUCCEEDED:
+    case types.API_V1_CUSTOMTEXT_READ_FAILED:
+      return Object.assign({}, state, {
+        tstcrAPI: [...state.tstcrAPI, action.response]
+      })
+    case types.API_V1_CUSTOMTEXT_UPDATE:
+    case types.API_V1_CUSTOMTEXT_UPDATE_SUCCEEDED:
+    case types.API_V1_CUSTOMTEXT_UPDATE_FAILED:
+      return Object.assign({}, state, {
+        tstcrAPI: [...state.tstcrAPI, action.response]
+      })
+    case types.API_V1_CUSTOMTEXT_PARTIAL_UPDATE:
+    case types.API_V1_CUSTOMTEXT_PARTIAL_UPDATE_SUCCEEDED:
+    case types.API_V1_CUSTOMTEXT_PARTIAL_UPDATE_FAILED:
+      return Object.assign({}, state, {
+        tstcrAPI: [...state.tstcrAPI, action.response]
+      })
+    case types.API_V1_HOMEPAGE_LIST:
+    case types.API_V1_HOMEPAGE_LIST_SUCCEEDED:
+    case types.API_V1_HOMEPAGE_LIST_FAILED:
+      return Object.assign({}, state, {
+        tstcrAPI: [...state.tstcrAPI, action.response]
+      })
+    case types.API_V1_HOMEPAGE_READ:
+    case types.API_V1_HOMEPAGE_READ_SUCCEEDED:
+    case types.API_V1_HOMEPAGE_READ_FAILED:
+      return Object.assign({}, state, {
+        tstcrAPI: [...state.tstcrAPI, action.response]
+      })
+    case types.API_V1_HOMEPAGE_UPDATE:
+    case types.API_V1_HOMEPAGE_UPDATE_SUCCEEDED:
+    case types.API_V1_HOMEPAGE_UPDATE_FAILED:
+      return Object.assign({}, state, {
+        tstcrAPI: [...state.tstcrAPI, action.response]
+      })
+    case types.API_V1_HOMEPAGE_PARTIAL_UPDATE:
+    case types.API_V1_HOMEPAGE_PARTIAL_UPDATE_SUCCEEDED:
+    case types.API_V1_HOMEPAGE_PARTIAL_UPDATE_FAILED:
+      return Object.assign({}, state, {
+        tstcrAPI: [...state.tstcrAPI, action.response]
+      })
+    case types.API_V1_LOGIN_CREATE:
+    case types.API_V1_LOGIN_CREATE_SUCCEEDED:
+    case types.API_V1_LOGIN_CREATE_FAILED:
+      return Object.assign({}, state, {
+        tstcrAPI: [...state.tstcrAPI, action.response]
+      })
+    case types.API_V1_SIGNUP_CREATE:
+    case types.API_V1_SIGNUP_CREATE_SUCCEEDED:
+    case types.API_V1_SIGNUP_CREATE_FAILED:
+      return Object.assign({}, state, {
+        tstcrAPI: [...state.tstcrAPI, action.response]
+      })
+    case types.REST_AUTH_LOGIN_CREATE:
+    case types.REST_AUTH_LOGIN_CREATE_SUCCEEDED:
+    case types.REST_AUTH_LOGIN_CREATE_FAILED:
+      return Object.assign({}, state, {
+        tstcrAPI: [...state.tstcrAPI, action.response]
+      })
+    case types.REST_AUTH_LOGOUT_LIST:
+    case types.REST_AUTH_LOGOUT_LIST_SUCCEEDED:
+    case types.REST_AUTH_LOGOUT_LIST_FAILED:
+      return Object.assign({}, state, {
+        tstcrAPI: [...state.tstcrAPI, action.response]
+      })
+    case types.REST_AUTH_LOGOUT_CREATE:
+    case types.REST_AUTH_LOGOUT_CREATE_SUCCEEDED:
+    case types.REST_AUTH_LOGOUT_CREATE_FAILED:
+      return Object.assign({}, state, {
+        tstcrAPI: [...state.tstcrAPI, action.response]
+      })
+    case types.REST_AUTH_PASSWORD_CHANGE_CREATE:
+    case types.REST_AUTH_PASSWORD_CHANGE_CREATE_SUCCEEDED:
+    case types.REST_AUTH_PASSWORD_CHANGE_CREATE_FAILED:
+      return Object.assign({}, state, {
+        tstcrAPI: [...state.tstcrAPI, action.response]
+      })
+    case types.REST_AUTH_PASSWORD_RESET_CREATE:
+    case types.REST_AUTH_PASSWORD_RESET_CREATE_SUCCEEDED:
+    case types.REST_AUTH_PASSWORD_RESET_CREATE_FAILED:
+      return Object.assign({}, state, {
+        tstcrAPI: [...state.tstcrAPI, action.response]
+      })
+    case types.REST_AUTH_PASSWORD_RESET_CONFIRM_CREATE:
+    case types.REST_AUTH_PASSWORD_RESET_CONFIRM_CREATE_SUCCEEDED:
+    case types.REST_AUTH_PASSWORD_RESET_CONFIRM_CREATE_FAILED:
+      return Object.assign({}, state, {
+        tstcrAPI: [...state.tstcrAPI, action.response]
+      })
+    case types.REST_AUTH_REGISTRATION_CREATE:
+    case types.REST_AUTH_REGISTRATION_CREATE_SUCCEEDED:
+    case types.REST_AUTH_REGISTRATION_CREATE_FAILED:
+      return Object.assign({}, state, {
+        tstcrAPI: [...state.tstcrAPI, action.response]
+      })
+    case types.REST_AUTH_REGISTRATION_VERIFY_EMAIL_CREATE:
+    case types.REST_AUTH_REGISTRATION_VERIFY_EMAIL_CREATE_SUCCEEDED:
+    case types.REST_AUTH_REGISTRATION_VERIFY_EMAIL_CREATE_FAILED:
+      return Object.assign({}, state, {
+        tstcrAPI: [...state.tstcrAPI, action.response]
+      })
+    case types.REST_AUTH_USER_READ:
+    case types.REST_AUTH_USER_READ_SUCCEEDED:
+    case types.REST_AUTH_USER_READ_FAILED:
+      return Object.assign({}, state, {
+        tstcrAPI: [...state.tstcrAPI, action.response]
+      })
+    case types.REST_AUTH_USER_UPDATE:
+    case types.REST_AUTH_USER_UPDATE_SUCCEEDED:
+    case types.REST_AUTH_USER_UPDATE_FAILED:
+      return Object.assign({}, state, {
+        tstcrAPI: [...state.tstcrAPI, action.response]
+      })
+    case types.REST_AUTH_USER_PARTIAL_UPDATE:
+    case types.REST_AUTH_USER_PARTIAL_UPDATE_SUCCEEDED:
+    case types.REST_AUTH_USER_PARTIAL_UPDATE_FAILED:
+      return Object.assign({}, state, {
+        tstcrAPI: [...state.tstcrAPI, action.response]
       })
     default:
       return state
