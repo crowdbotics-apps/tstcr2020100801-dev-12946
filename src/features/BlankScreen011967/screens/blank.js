ac174e2e9c293dc60a868c21e4e47cf20190197b
Daniel <daniel.s@crowdbotics.com>
2020-10-13 16:51:04 +0100

    Show users and registration state objects in screen

--- src/features/BlankScreen011967/screens/blank.js
+++ src/features/BlankScreen011967/screens/blank.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { connect } from "react-redux";
-import { registration_post_restauth_registration_create } from "../../../store/actions.js";
+import React from 'react';
+import {connect} from 'react-redux';
+import {registration_post_restauth_registration_create} from '../../../store/actions.js';
 import {
   View,
   Image,
@@ -10,54 +10,65 @@ import {
   Button,
   Switch,
   TextInput,
-  StyleSheet
-} from "react-native";
+  StyleSheet,
+} from 'react-native';
 class Blank extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: "",
-      password: ""
+      email: '',
+      password: '',
     };
   }
   render = () => (
     <View style={styles.container}>
       <Text>Welcome to Crowdbotics</Text>
       <TextInput
-        onChangeText={email => this.setState({ email: email })}
-        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+        onChangeText={email => this.setState({email: email})}
+        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
       />
       <TextInput
-        onChangeText={password => this.setState({ password: password })}
-        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+        onChangeText={password => this.setState({password: password})}
+        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
       />
       <TouchableOpacity
         style={styles.button}
-        onPress={() => this.props.signup(this.state.email, this.state.password)}
-      >
+        onPress={() =>
+          this.props.signup(this.state.email, this.state.password)
+        }>
         <Text>Signup</Text>
       </TouchableOpacity>
+      <Text>User count {this.props.users.length}</Text>
+      <Text>Errors {this.props.registration.message}</Text>
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginHorizontal: 16
+    marginHorizontal: 16,
   },
   button: {
-    alignItems: "center",
-    backgroundColor: "#DDDDDD",
-    padding: 10
-  }
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+  },
 });
 function mapStateToProps(state) {
-  return { state: state };
+  return {
+    users: state.apiReducer.users,
+    registration: state.apiReducer.registration,
+  };
 }
 const mapDispatchToProps = dispatch => {
   return {
     signup: (email, password) =>
-      dispatch(registration_post_restauth_registration_create({email, password}))
+      dispatch(
+        registration_post_restauth_registration_create({email, password}),
+      ),
   };
 };
-export default connect(mapStateToProps, mapDispatchToProps)(Blank);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Blank);
--- src/store/services.js
+++ src/store/services.js
@@ -1,97 +1,99 @@
-import axios from "axios"
+import axios from 'axios';
 const test = axios.create({
-  baseURL: "http://879976904ff5.ngrok.io",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  baseURL: 'http://879976904ff5.ngrok.io',
+  headers: {Accept: 'application/json', 'Content-Type': 'application/json'},
+});
 const testConnector = axios.create({
-  baseURL: "http://da3097b9b2b0.ngrok.io/",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  baseURL: 'http://da3097b9b2b0.ngrok.io/',
+  headers: {Accept: 'application/json', 'Content-Type': 'application/json'},
+});
 const registration = axios.create({
-  baseURL: "https://tstcr2020100801-dev-12946.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  baseURL: 'https://23b6dee078b0.ngrok.io',
+  headers: {Accept: 'application/json', 'Content-Type': 'application/json'},
+});
 const tstcrAPI = axios.create({
-  baseURL: "https://tstcr2020100801-dev-12946.botics.co/",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  baseURL: 'https://tstcr2020100801-dev-12946.botics.co/',
+  headers: {Accept: 'application/json', 'Content-Type': 'application/json'},
+});
 function test2_post__create(action) {
-  return test.post(`/`, null, { data: action.data })
+  return test.post(`/`, null, {data: action.data});
 }
 function testconnector_post__create(action) {
-  return testConnector.post(`/`, { data: action.data })
+  return testConnector.post(`/`, {data: action.data});
 }
 function registration_post_restauth_registration_create(action) {
-  return registration.post(`/rest-auth/registration/`, null, { data: action.data })
+  return registration.post(`/rest-auth/registration/`, null, {
+    data: action.data,
+  });
 }
 function api_v1_customtext_list(action) {
-  return tstcrAPI.get(`/api/v1/customtext/`)
+  return tstcrAPI.get(`/api/v1/customtext/`);
 }
 function api_v1_customtext_read(action) {
-  return tstcrAPI.get(`/api/v1/customtext/${action.id}/`)
+  return tstcrAPI.get(`/api/v1/customtext/${action.id}/`);
 }
 function api_v1_customtext_update(action) {
-  return tstcrAPI.put(`/api/v1/customtext/${action.id}/`, { data: action.data })
+  return tstcrAPI.put(`/api/v1/customtext/${action.id}/`, {data: action.data});
 }
 function api_v1_customtext_partial_update(action) {
   return tstcrAPI.patch(`/api/v1/customtext/${action.id}/`, {
-    data: action.data
-  })
+    data: action.data,
+  });
 }
 function api_v1_homepage_list(action) {
-  return tstcrAPI.get(`/api/v1/homepage/`)
+  return tstcrAPI.get(`/api/v1/homepage/`);
 }
 function api_v1_homepage_read(action) {
-  return tstcrAPI.get(`/api/v1/homepage/${action.id}/`)
+  return tstcrAPI.get(`/api/v1/homepage/${action.id}/`);
 }
 function api_v1_homepage_update(action) {
-  return tstcrAPI.put(`/api/v1/homepage/${action.id}/`, { data: action.data })
+  return tstcrAPI.put(`/api/v1/homepage/${action.id}/`, {data: action.data});
 }
 function api_v1_homepage_partial_update(action) {
-  return tstcrAPI.patch(`/api/v1/homepage/${action.id}/`, { data: action.data })
+  return tstcrAPI.patch(`/api/v1/homepage/${action.id}/`, {data: action.data});
 }
 function api_v1_login_create(action) {
-  return tstcrAPI.post(`/api/v1/login/`)
+  return tstcrAPI.post(`/api/v1/login/`);
 }
 function api_v1_signup_create(action) {
-  return tstcrAPI.post(`/api/v1/signup/`, { data: action.data })
+  return tstcrAPI.post(`/api/v1/signup/`, {data: action.data});
 }
 function rest_auth_login_create(action) {
-  return tstcrAPI.post(`/rest-auth/login/`, { data: action.data })
+  return tstcrAPI.post(`/rest-auth/login/`, {data: action.data});
 }
 function rest_auth_logout_list(action) {
-  return tstcrAPI.get(`/rest-auth/logout/`)
+  return tstcrAPI.get(`/rest-auth/logout/`);
 }
 function rest_auth_logout_create(action) {
-  return tstcrAPI.post(`/rest-auth/logout/`)
+  return tstcrAPI.post(`/rest-auth/logout/`);
 }
 function rest_auth_password_change_create(action) {
-  return tstcrAPI.post(`/rest-auth/password/change/`, { data: action.data })
+  return tstcrAPI.post(`/rest-auth/password/change/`, {data: action.data});
 }
 function rest_auth_password_reset_create(action) {
-  return tstcrAPI.post(`/rest-auth/password/reset/`, { data: action.data })
+  return tstcrAPI.post(`/rest-auth/password/reset/`, {data: action.data});
 }
 function rest_auth_password_reset_confirm_create(action) {
   return tstcrAPI.post(`/rest-auth/password/reset/confirm/`, {
-    data: action.data
-  })
+    data: action.data,
+  });
 }
 function rest_auth_registration_create(action) {
-  return tstcrAPI.post(`/rest-auth/registration/`, { data: action.data })
+  return tstcrAPI.post(`/rest-auth/registration/`, {data: action.data});
 }
 function rest_auth_registration_verify_email_create(action) {
   return tstcrAPI.post(`/rest-auth/registration/verify-email/`, {
-    data: action.data
-  })
+    data: action.data,
+  });
 }
 function rest_auth_user_read(action) {
-  return tstcrAPI.get(`/rest-auth/user/`)
+  return tstcrAPI.get(`/rest-auth/user/`);
 }
 function rest_auth_user_update(action) {
-  return tstcrAPI.put(`/rest-auth/user/`, { data: action.data })
+  return tstcrAPI.put(`/rest-auth/user/`, {data: action.data});
 }
 function rest_auth_user_partial_update(action) {
-  return tstcrAPI.patch(`/rest-auth/user/`, { data: action.data })
+  return tstcrAPI.patch(`/rest-auth/user/`, {data: action.data});
 }
 export const apiService = {
   test2_post__create,
@@ -117,5 +119,5 @@ export const apiService = {
   rest_auth_registration_verify_email_create,
   rest_auth_user_read,
   rest_auth_user_update,
-  rest_auth_user_partial_update
-}
+  rest_auth_user_partial_update,
+};
